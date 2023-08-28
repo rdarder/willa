04977033d139d039787b5451161c9357a1e3730a
Rafael Darder <darder@gmail.com>
2023-08-27 23:30:41 -0300

    mock websocket server for testing the webapp / controller locally

---
+++ willa-mock-controller/src/server.ts
@@ -0,0 +1,21 @@
+import WebSocket from 'ws';
+import * as flatbuffers from "flatbuffers";
+import {Willa}  from "./serialization";
+
+
+const wss = new WebSocket.WebSocketServer({ port: 5000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("connection attempt!");
+  ws.on("error", console.error);
+
+  ws.on("message", function message(message: Buffer) {
+    let buffer = new flatbuffers.ByteBuffer(message)
+    const control = Willa.DriveController.DriveControl.getRootAsDriveControl(buffer as any);
+    console.log(JSON.stringify({
+      steer: control.steer(),
+      motorMode: control.motorMode(),
+      motorPower: control.motorPower(),
+    }))  
+  });
+});
