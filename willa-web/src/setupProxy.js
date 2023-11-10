68003d6c31ed1925023889e1bbd38bdd6d66d8f8
Rafael Darder <darder@gmail.com>
2023-11-10 10:39:15 -0300

    remove now deprecated react web app in favor of lit's app in willa-app

--- willa-web/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-console.log("setupProxy.js");
-
-module.exports = function (app) {
-  app.use("/controller", createProxyMiddleware("ws://localhost:5000"));
-};
